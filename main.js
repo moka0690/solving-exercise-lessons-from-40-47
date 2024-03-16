bf0006dc86766147399d7f9f76e3e30d1cb851ed
moka0690 <madradz39@gmail.com>
2024-03-16 23:11:48 +0100

    solveSolving exercise 1

---
+++ main.js
@@ -0,0 +1,11 @@
+let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
+let num = 3;
+
+
+myFriends.length =num
+// Method 1
+console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
+
+// Method 2
+console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
+myFriends.pop()
