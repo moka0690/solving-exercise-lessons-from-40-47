14fece800c4797130c2f03b3714f9aeb9b6d91f2
moka0690 <madradz39@gmail.com>
2024-03-17 21:20:15 +0100

    solveSolving exercise 1 Correcting a typo

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
 let num = 3;
 
 
-myFriends.length =num
+
+myFriends.pop()
 // Method 1
 console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
 
+myFriends.length =num
 // Method 2
 console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
-myFriends.pop()
