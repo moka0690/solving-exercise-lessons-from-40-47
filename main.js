4ba60ac733601b59846fb1b5a0aff498e94fb13e
moka0690 <madradz39@gmail.com>
2024-03-19 21:13:17 +0100

    solveSolving exercise 3

--- main.js
+++ main.js
@@ -18,3 +18,13 @@ let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
 friends.pop(friends.shift())
 // Method 1
 console.log(friends); // ["Eman", "Osama"] 
+
+
+
+let arrOne = ["C", "D", "X"];
+let arrTwo = ["A", "B", "Z"];
+let finalArr = [];
+finalArr = arrOne.concat(arrTwo).sort().reverse()
+// Write One Single Line Of Code
+
+console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
